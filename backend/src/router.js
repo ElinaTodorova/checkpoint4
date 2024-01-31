ec2653febec3611f329a97afbdd60035c5380d55
ElinaTodorova <elina.todorova93@gmail.com>
2024-01-31 10:54:23 +0100

    ReadAll Gift and Activity

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,9 +8,13 @@ const router = express.Router();
 
 // Import itemControllers module for handling item-related operations
 const itemControllers = require("./controllers/itemControllers");
+const giftControllers = require("./controllers/giftControllers");
+const activityControllers = require("./controllers/activityControllers");
 
 // Route to get a list of items
 router.get("/items", itemControllers.browse);
+router.get("/gifts", giftControllers.browse);
+router.get("/activities", activityControllers.browse);
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemControllers.read);

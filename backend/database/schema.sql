66404d5db8e1a5b68f8540768d1693402179333f
ElinaTodorova <elina.todorova93@gmail.com>
2024-01-31 10:18:11 +0100

    Creation et alimentation DB

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,34 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  username VARCHAR(150) NOT NULL,
+  hashed_password VARCHAR(255)NOT NULL
 );
+
+DROP TABLE IF EXISTS gift;
+CREATE TABLE gift(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name_gift VARCHAR(255) NOT NULL,
+  description_gift TEXT,
+  age_min INT NOT NULL,
+  image_url VARCHAR(255),
+  user_id INT,
+  CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+DROP TABLE IF EXISTS activity;
+CREATE TABLE activity(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name_activity VARCHAR(255) NOT NULL,
+  description_activity TEXT,
+  age_min INT NOT NULL,
+  age_max INT NOT NULL,
+  image_url VARCHAR(255),
+  userActivity_id INT,
+  CONSTRAINT userActivity_id FOREIGN KEY (userActivity_id) REFERENCES user(id)
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
--- backend/seed.js
+++ backend/seed.js
@@ -4,7 +4,7 @@
 require("dotenv").config();
 
 // Import Faker library for generating fake data
-const { faker } = require("@faker-js/faker");
+// const { faker } = require("@faker-js/faker");
 
 // Import database client
 const database = require("./database/client");
@@ -18,18 +18,37 @@ const seed = async () => {
     /* ************************************************************************* */
 
     // Generating Seed Data
+    await database.query("delete from gift");
+    queries.push(
+      database.query(`INSERT INTO gift (name_gift, description_gift, age_min, image_url) 
+      VALUES
+      ('Barbie Doll', 
+      'A beautiful Barbie doll with blonde hair and blue eyes, dressed in an elegant pink gown. It''s the perfect gift for little girls who enjoy playing with dolls.', 
+      3, 'images/barbie.jpg'),
+      ('Hape Penguin Musical Wobbler',
+      'A delightful penguin-shaped musical wobbler by Hape. This toy features a cute penguin design that wobbles and plays a soothing melody when pushed. Perfect for encouraging early walking and providing sensory stimulation for little ones. Crafted from high-quality, child-safe materials for hours of musical fun!', 
+      0, 'images/babyToy')`)
+    );
+
+    await database.query("delete from activity");
+    queries.push(
+      database.query(`INSERT INTO activity (name_activity, description_activity, age_min, age_max, image_url)
+      VALUES 
+      ('Finger Painting', 'Let your child''s creativity soar with our delightful Finger Painting activity! Perfect for little artists, this hands-on experience encourages self-expression and imagination. Kids will enjoy exploring different colors and creating their own masterpieces using their fingers as paintbrushes.', 1, 6, 'images/fingerPainting.jpg' ),
+      ('Puppet Theater', 'Create a small puppet theater and encourage children to invent and perform their own stories.', 2, 8, 'images/puppet.jpg')`)
+    );
 
     // Optional: Truncate tables (remove existing data)
-    await database.query("truncate item");
+    // await database.query("truncate item");
 
     // Insert fake data into the 'item' table
-    for (let i = 0; i < 10; i += 1) {
-      queries.push(
-        database.query("insert into item(title) values (?)", [
-          faker.lorem.word(),
-        ])
-      );
-    }
+    // for (let i = 0; i < 10; i += 1) {
+    //   queries.push(
+    //     database.query("insert into item(title) values (?)", [
+    //       faker.lorem.word(),
+    //     ])
+    //   );
+    // }
 
     /* ************************************************************************* */
 

8954465d173745351371376320856066dad46b74
ElinaTodorova <elina.todorova93@gmail.com>
2024-05-30 21:37:56 +0200

    Config database

--- backend/database/client.js
+++ backend/database/client.js
@@ -1,22 +1,23 @@
 // Get variables from .env file for database connection
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { MYSQLDATABASE, MYSQLHOST, MYSQLPORT, MYSQLUSER, MYSQLPASSWORD } =
+  process.env;
 
 // Create a connection pool to the database
 const mysql = require("mysql2/promise");
 
 const client = mysql.createPool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  host: MYSQLHOST,
+  port: MYSQLPORT,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
 });
 
 // Try to get a connection to the database
 client
   .getConnection()
   .then((connection) => {
-    console.info(`Using database ${DB_NAME}`);
+    console.info(`Using database ${MYSQLDATABASE}`);
 
     connection.release();
   })
@@ -30,7 +31,7 @@ client
   });
 
 // Store database name into client for further uses
-client.databaseName = DB_NAME;
+client.databaseName = MYSQLDATABASE;
 
 // Ready to export
 module.exports = client;
